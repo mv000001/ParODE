0a8e736cf5c17946df1d5b31e5a5b2d9754b1610
mv000001 <mv000001@gmail.com>
2025-10-13 20:15:23 +0200

    Update parode_example.py

--- parode_example.py
+++ parode_example.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
         return -y**3 + torch.sin(t)
 
     solver = ParODE(M=10, N=10, K=200000)
-    t_grid = torch.linspace(0.0, 0.1, 201, device=solver.device, dtype=solver.dtype)
+    t_grid = torch.linspace(0.0, 2.0, 201, device=solver.device, dtype=solver.dtype)
     y0 = 0.5
     y_pred, A = solver.solve(f_rhs, y0, t_grid)
     solver.compare_to_rk4(f_rhs, y0, t_grid, y_pred)
